5b229e3b7acdeee13c813ce1d4cffa0ee284098d
rocketII <vangandr@gmail.com>
2016-12-11 11:47:51 +0100

    Tilfid: working on encryption.

--- main.py
+++ main.py
@@ -3,20 +3,22 @@
 # 1         make one cipher work at the time
 # 2
 # Do not use this old ciphers with important secrets. They are old and can be forced open using math just a moment of days or weeks.
+#Thinking what are they? Just read wikipedia or search for them on the internet.
 import playfair
 import adfvgx
 import tilfid
 import dubbleTranspose
+print"------------------------------------"
 print "Demo playfair, remember i = j"
 print"------------------------------------"
 clearTxt = ['j','a','g','a','t','e','r']
-print clearTxt
+print "My secret: ",clearTxt
 nyckel = "skitgubbegillarfarskfisk"
 encrypted1 = playfair.playfair_encrypt(clearTxt,nyckel,'n')
-print encrypted1
+print "My encrypted secret: ",encrypted1
 
 secret =playfair.playfair_decryption(encrypted1,nyckel,'n')
-print secret
+print "My secret revealed: ",secret
 print"------------------------------------"
 print "Demo ADFVGX"
 print"------------------------------------"
@@ -28,5 +30,9 @@ print "secret: ",secretStuff
 encryptStuff = adfvgx.adfgvx_encryption(key, secretStuff)
 print "encrypted secret: ",encryptStuff
 revealed = adfvgx.adfgvx_decryption(key, encryptStuff)
-print "message: " , revealed
+print "Decrypted message: " , revealed
+print"------------------------------------"
+print "Demo Tilfid"
+print"------------------------------------"
+tilfid.tilfid_encryption("secret")
 print"------------------------------------"
--- tilfid.py
+++ tilfid.py
@@ -1,3 +1,47 @@
-def rob ():
-    print "test this is rob!"
+def tilfid_encryption (secret_Stuff):
+    '''
+    Encrypt your message with tilfid cipher
+    :param secret_Stuff: enter text string "secret stuff"
+    :return: encrypted stuff
+    '''
+    secret = secret_Stuff
+    secretLength = len(secret_Stuff)
+    helper = ['1','2','3']
+    encrypted_tmp = []
+    for n in range(secretLength):
+        encrypted_tmp.append([None, None, None])
+    encrypted = []
+    #Using coordinates layer,row, col
+    #Make sure that booth recipient and sender got the same layers like below. Modify when required.
+    layer1 = [['a','k','g'], ['l','r','t'], ['x','m','n']]
+    layer2 = [['q', 'w', 'm'], ['!', 'o', 'e'], ['u', 's', 'z']]
+    layer3 = [['i', 'f', 'v'], ['j', 'p', 'd'], ['b', 'h', 'c']]
+    master = [layer1, layer2, layer3]
+    print master[1][2][2]
+    # letter by letter read and encode using master and secret
+    for i in range(secretLength):
+        for layer in range(3):
+                for col in range(3):
+                    for row in range(3):
+                        #sorry this is totally quadratic time complexity x^4... when there's time I should make it better, hashtable or something
+                        if master[layer][col][row] == secret[i]:
+                            encrypted_tmp[i][0] = layer
+                            encrypted_tmp[i][1] = row
+                            encrypted_tmp[i][2] = col
+    print "Encoded: ", encrypted_tmp
+    # read row by row, trigram gets encoded and saved.
+    #rowByrow = [None] * secretLength
+    trigram = [None] * 3
+    li = 0; ci =0; ri = 0; j = 0
+    for round in range(secretLength):
+            trigram[j] = encrypted_tmp[ci][0]
+            ci = (ci + 1) % secretLength;
+            trigram[j] = encrypted_tmp[ci][1]
+            ci = (ci + 1) % secretLength;
+            trigram[j] = encrypted_tmp[ci][2]
+            ci = (ci + 1) % secretLength;
+            #                        layer        row         col
+            encrypted.append(master[trigram[0]][trigram[1]][trigram[2]])
+    print "Encrypted: ", encrypted
+
     return
