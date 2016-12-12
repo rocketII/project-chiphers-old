80fc46c21d6d7d188d8237853ce2ccb1a4892443
rocketII <vangandr@gmail.com>
2016-12-12 07:41:10 +0100

    Tilfid: decrypt and encryption works for up to eleven characters. Dubble Transpose:  encryption missing column sorting and some tests,

--- dubbleTranspose.py
+++ dubbleTranspose.py
@@ -1,34 +1,54 @@
 def deb ():
-    print "deb goes meh!"
+    print ".deb files goes meh! So dependant"
     return
 
 def dubbleTransposse_encryption(): # (key, secret):
     '''
     takes key and secret creates cipher text and encrypt it too then it returns the result
     :param key:
-    :param secret:
-    :return:
+    :param secret: the use of char -1 is strictly forbidden!
+    :return: gibberish whitch require decryption in order to read
     '''
     #calc nr of filled rows, nr of empty row slots and nr of col.
-    keyHold = "abc" #key
+    encyptionList = ['-1']
+    keyHold = "abcde" #key
     keyLength = len(keyHold)
-    cache = "aaaaaaaa" # secret
+    cache = "abcdefghijk" # secret
     cacheLength = len(cache)
     Fullrows = cacheLength / keyLength
     nrOfEmptySpaces = (keyLength -(cacheLength % keyLength))
+    if nrOfEmptySpaces > 0:
+        Fullrows += 1
+    numberOfElements = keyLength * Fullrows
     # Generate empty 2D list with n rows and m col.
-    master = [[None]]
+    master = []
     for i in range(keyLength):
-        master
+        master.insert(0 , [])
+    for j in range((numberOfElements)):
+        master[(j % keyLength)].append('-1')
     master
     print master
 
     # Fill 2D list with elements row by row. When empty string apply the rest of empty spaces
+    row = 0; col = 0
+    for h in range((cacheLength)):
+        master[col][row] = cache[h]
+        col = (col + 1) % keyLength
+        if col == 0:
+            row = (row + 1) % Fullrows
 
     # Sort the 2D list columns chronological with a as the lowest at the left side.
 
     # Read col for col where we skip 'special char None or -1' into the encryption list/string and return result
-
+    row = 0; col = 0
+    for e in range(numberOfElements):
+        if master[col][row] != '-1':
+            encyptionList.insert(0, master[col][row])
+        row = (row + 1) % Fullrows
+        if row == 0:
+            col = (col + 1) % keyLength
+    encyptionList.reverse()
+    encyptionList.remove('-1')
     return 0
 
 def dubbleTransposse_decryption(): # (key, encrypted_text)
