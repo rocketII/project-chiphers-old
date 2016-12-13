6a595e8d60fb6b6bc5d740ba03d42ba3126387c2
rocketII <vangandr@gmail.com>
2016-12-13 09:22:29 +0100

    Tilfid: decrypt and encryption works for up to eleven characters. BUG 002 to be solved sometimes in the near future... Dubble Transpose:            Decryption: Made the logic work for some cases. Almost done with the           code

--- dubbleTranspose.py
+++ dubbleTranspose.py
@@ -91,17 +91,76 @@ for a in range(5):  # range is nrOfCol
 '''
 def dubbleTransposse_decryption(): # (key, encrypted_text)
     # calc nr of filled rows, nr of empty row slots and nr of col.
-
+    decryptionList = ['-1']
+    keyHolder = "bac" # default "ab"
+    keyLength = len(keyHolder)  # makes columns
+    encryptedMessg = ['a', 'c', 'e', 'g', 'i', 'k', 'b', 'd', 'f', 'h', 'j'] # should get abcdefghijk as result
+    encryptedMessgLength = len(encryptedMessg)
+    Fullrows = encryptedMessgLength / keyLength  # contains soon total rows exkl row 0
+    nrOfEmptySpaces = (keyLength - (encryptedMessgLength % keyLength))
+    if nrOfEmptySpaces > 0:
+        Fullrows += 1
+    numberOfElements = keyLength * Fullrows
     # Generate empty 2D list with n rows and m col. (calc nr of rows, keylength == nrOfCol)
-
-    # Mark the loweest row elements from the lowest right col
-
+    master = []
+    for i in range(keyLength):
+        master.insert(0, [])
+    for j in range((numberOfElements + 1)):
+        if j == 0:
+            for p in range(keyLength):
+                master[p].append(keyHolder[p])
+        else:
+            master[(j % keyLength)].append('-1')
+    # Mark the loweest row elements from the lowest right col      <--------------------- untill here it works
+    countDown = keyLength - 1
+    for mark in range(nrOfEmptySpaces):
+        master[countDown][(Fullrows)] = '-2'
+        countDown -= 1
     # sort the cols alphabetically
+    master.sort()
+    # Read the elements into the 2D list and jump '-2' spaces. column by column
+    row = 1;col = 0
+    for h in range(encryptedMessgLength):
 
-    # Read the elements into the 2D list and jump empty spaces.
+        if row == 0 and master[col][row] != '-2':
+            row = (row % Fullrows) + 1
+        master[col][row] = encryptedMessg[h]
+        row = (row % Fullrows) + 1
+        if row == 1:
+            col = (col + 1) % keyLength
+    # swap cols so that the col headers match the key       <------------ works but needs more tests
 
-    #swap cols so that the col headers match the key
+    # used to swap columns
+    tmpCol = [['+']]; tmp2Col = [['+']]
+    for round in range(Fullrows):
+        tmpCol[0].append('-')
+    for round in range(Fullrows):
+        tmp2Col[0].append('-')
 
+    for a in range(keyLength):  # range is nrOfCol
+        for b in range(keyLength):  # range is nrOfCol
+            if keyHolder[a] == master[b][0]:
+                if keyLength == 2 and a == 1:
+                    continue
+                elif keyLength == 1:
+                    continue
+                else:
+                    for f in range(Fullrows + 1):  # range depends on nr of rows because f walks throug the rows in the columns. +1 with fullrows covers the header row
+                        tmpCol[0][f] = master[a][f]
+                        tmp2Col[0][f] = master[b][f]
+                        # copy a col into tmpCol, copy e col into tmp2Col
+                        # Now tmpCol overwrite e col
+                        master[b][f] = tmpCol[0][f]
+                        # Now tmp2Col overwrite a col
+                        master[a][f] = tmp2Col[0][f]
     # read row by row into Decypted string and return it.
-
+    row = 1;col = 0
+    for e in range(numberOfElements):
+        if (master[col][row] != '-2') and row != 0:
+            decryptionList.insert(0, master[col][row])
+        col = (col % keyLength) + 1
+        if col == 1:
+            row = (row % Fullrows)  + 1
+    decryptionList.reverse()
+    decryptionList.remove('-1')
     return
