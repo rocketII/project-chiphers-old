0818e183015d83863b91e59d49cf00cbf465188a
rocketII <vangandr@gmail.com>
2016-12-19 15:17:46 +0100

    ADFVGX: working on encryption (only half of the cipher.) Tilfid: decrypt and encryption works for up to eleven characters. BUG 002 to be solved sometimes in the near future... Dubble Transpose:            Decryption: It seems to me that we are maybe done. Next: (optional)Optimize the algorithms to increase time complexity. It's not            nice to run three or more nested loops.           Write sagemathcloud document with the code.

--- adfvgx.py
+++ adfvgx.py
@@ -1,16 +1,17 @@
 # Oops! I only did half of the chipher... fix it soon.
-def adfgvx_encryption(keyUsing36char, clearTextUsingTheKeyForCoding):
+def adfgvx_encryption(keyUsing36char, key24transposing , clearTextUsingTheKeyForCoding):
     '''
-
+    ADFGVX famous old cipher, Need more info read some books ;-)
     :param keyUsing36char: must include 0 to 9 and a to z and shuffle it. Send it as "string"
+    :param key24transposing:
     :param clearTextUsingTheKeyForCoding:  send it as ['s','c','r','e','t']
     :return: encryptet message
     '''
     secret = clearTextUsingTheKeyForCoding
-    key = keyUsing36char
     secretLength = len(secret)
     encrypted = []
     adfgvx = "adfgvx"
+
     # create 6x6 matrix using lists
     col0 = [None] * 6
     col1 = [None] * 6
@@ -24,7 +25,8 @@ def adfgvx_encryption(keyUsing36char, clearTextUsingTheKeyForCoding):
         for col in range(0,6,1):
             master[col][row] = keyUsing36char[i]
             i += 1
-    # use key char by char and decode, read col first then row.
+
+    # use key char by char and decode, read row by row.
     i = 0
     for i in  range(secretLength):
         for row in range(6):
@@ -32,18 +34,66 @@ def adfgvx_encryption(keyUsing36char, clearTextUsingTheKeyForCoding):
                 if( master[col][row] == secret[i]):
                     encrypted.insert(0, adfgvx[col])
                     encrypted.insert(0, adfgvx[row])
-    encrypted.reverse()
+    encrypted.reverse()  # because above algorithm works backwards.
+    # halfway, now purge trash.
+    del i, secretLength, secret, master, col0, col1, col2, col3, col4, col5
+    # make table with key length as nrOfColumns, calc the nr of slots needed and then calc nr of rows. no empty slots needed.
+    # Generate empty 2D list with n rows and m col.
+    key = key24transposing
+    keylength = len(key)
+    encryptedlength = len(encrypted)
+    Fullrows = encryptedlength / keylength
+    nrOfEmptySpaces = (keylength - (encryptedlength % keylength))
+    if nrOfEmptySpaces > 0:
+        Fullrows += 1
+    numberOfElements = keylength * Fullrows
+
+
+    masterII = []
+
+    # walksthrough key char by char. and makes columns
+    for i in range(keylength):
+        masterII.insert(0, [])
+    # for every char in the table make the title row filled with key. else fill the table row by row with -1.
+
+    for p in range(keylength):
+        masterII[p].append(key[p])
+    for j in range((numberOfElements)):
+        masterII[(j % keylength)].append('-1')
+    del adfgvx, i, j, p
+    # Enter the content of "encrypted" row by row.
+        # Fill 2D list with elements row by row. When empty string apply the rest of empty spaces
+        # the row 0 are only for sorting and column swapping. Never read it as part in encryption message.
+    row = 1
+    col = 0
+
+    for h in range(encryptedlength):
+
+            if row == 0:
+                row = (row % Fullrows) + 1
+            masterII[col][row] = encrypted[h] # try dynamic column growth.
+            col = (col + 1) % keylength
+            if col == 0:
+                row = (row % Fullrows) + 1
+    # sort the table headers chronological.
+    masterII.sort()
+    # <---------------------------Move me to the end of the recent working area!!!!!!!
+    # read column for column chronological and then remove -1 from the string. return the result.
+
     return encrypted
 
-def adfgvx_decryption(keyUsing36char,encryptedMessage):
+def adfgvx_decryption(keyUsing36char, key24transposing, encryptedMessage):
     '''
-
+    ADFGVX, decryption
     :param keyUsing36char: must include 0 to 9 and a to z and shuffle it. Send it as "string"
+    :param key24transposing:
     :param encryptedMessage: the message length % 2 should be 0. If 1 then it's broken.
     :return: Secret
     '''
+
     #create chart
     # create 6x6 matrix using lists
+    key2 = key24transposing
     col0 = [None] * 6
     col1 = [None] * 6
     col2 = [None] * 6
--- main.py
+++ main.py
@@ -1,37 +1,50 @@
 # encoding: utf-8
-#implement them in order
+
+#implement them in prio order
 #Priority   text
-# 1         make one cipher work at the time
-# 2
+# 1         finish ADFVGX
+#2          analysis of known bugs
 # Do not use this old ciphers with important secrets. They are old and can be forced open using math just a moment of days or weeks.
 #Thinking what are they? Just read wikipedia or search for them on the internet.
+
 import playfair
 import adfvgx
 import tilfid
 import dubbleTranspose
+print "Disclaimer: These ciphers are not meant to be used with important data. I take no responsibility for this code. "
 print"------------------------------------"
 print "Demo playfair, remember i = j"
 print"------------------------------------"
 clearTxt = ['j','a','g','a','t','e','r']
 print "My secret: ",clearTxt
-nyckel = "skitgubbegillarfarskfisk"
+nyckel = "runningaround"
 encrypted1 = playfair.playfair_encrypt(clearTxt,nyckel,'n')
 print "My encrypted secret: ",encrypted1
-
-secret =playfair.playfair_decryption(encrypted1,nyckel,'n')
+secret = playfair.playfair_decryption(encrypted1,nyckel,'n')
 print "My secret revealed: ",secret
+
+
+
+
+
 print"------------------------------------"
 print "Demo ADFVGX"
 print"------------------------------------"
+
 key= "0n1o2p3q4a5b6c7d8e9rstuvwxyzfghjiklm"
+colKey = "shangrila"
 print "key length: ",len(key)
 secretStuff = ['i','c','a','n','d','r','i','n','k','m','i','l','k']
 length = len(secretStuff)
 print "secret: ",secretStuff
-encryptStuff = adfvgx.adfgvx_encryption(key, secretStuff)
+encryptStuff = adfvgx.adfgvx_encryption(key, colKey, secretStuff)
 print "encrypted secret: ",encryptStuff
-revealed = adfvgx.adfgvx_decryption(key, encryptStuff)
+revealed = adfvgx.adfgvx_decryption(key, colKey, encryptStuff)
 print "Decrypted message: " , revealed
+
+
+
+
 print"------------------------------------"
 print "Demo Tilfid"
 print"------------------------------------"
@@ -43,13 +56,21 @@ print "Secret message:       ", swane," length: ", len(swane)
 cloud = tilfid.tilfid_encryption(swane)
 print "Encrypted message     ", cloud
 print "Decrypted encryption: ",tilfid.tilfid_decryption(cloud)
+
+
+
 print"------------------------------------"
 print "Demo Dubble Transpose"
 print"------------------------------------"
-passwd = "juliabbbaRosmarieqwertyuiopasdfghjkl"   #"abcdefghijk"
-key = "bawkeyx"
+passwd = "juliaAbbaRosmarieQwertyBobSimon"   #"abcdefghijk"
+key = "bawkeyxzgh"
 print "From passwd=",passwd," and key=",key
-cloudMyCloud = dubbleTranspose.dubbleTransposse_encryption(key, passwd)
-print "We get encrypted code: ",cloudMyCloud
-print "We get decrypted code: ",dubbleTranspose.dubbleTransposse_decryption(key, cloudMyCloud)
+first = dubbleTranspose.dubbleTransposse_encryption(key, passwd)
+cloudMyCloud = dubbleTranspose.dubbleTransposse_encryption(key, first)
+print "We get encrypted code1: ",first
+print "We get encrypted code2: ",cloudMyCloud
+decryptR1 = dubbleTranspose.dubbleTransposse_decryption(key, cloudMyCloud)
+decryptR2 = dubbleTranspose.dubbleTransposse_decryption(key, decryptR1)
+print "We get decrypted code1: ", decryptR1
+print "We get decrypted code2: ", decryptR2
 print"------------------------------------"
